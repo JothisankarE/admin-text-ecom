ec3366bb38c5918614158e725d3b1b224ae8ef9f
karthikeyan_p <dkarthideepak@gmail.com>
2026-03-12 10:18:16 +0530

    feat: Add Vite configuration for the React admin project, including base path and build output settings.

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/admin/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: process.env.VITE_ADMIN_SUBPATH === 'true' ? '/admin/' : '/',
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+    }
   }
 })
